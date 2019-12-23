93bc50c15e84d6da4b51d644444bf2fe799179b6
Marat <shaimardanov@infomatika.ru>
2019-12-23 14:45:14 +0300

    Update gallery

--- parts/gallery.php
+++ parts/gallery.php
@@ -1,22 +1,24 @@
 <div class="slider">
     <div class="slider-inner" id="js-slider">
         <div>
-            <a href="./images/rock.jpg" data-fancybox data-caption="Описание картинки">
+            <a href="./images/rock.jpg" data-fancybox="group" data-caption="Описание картинки" >
                 <img src="./images/rock.jpg" alt="" />
             </a>
+
         </div>
         <div>
-            <a href="./images/80.jpg" data-fancybox data-caption="Описание картинки 2">
+            <a href="./images/80.jpg" data-fancybox="group" data-caption="Описание картинки 2">
                 <img src="./images/80.jpg" alt="" />
             </a>
         </div>
         <div>
-            <a href="./images/battle.jpg" data-fancybox data-caption="Описание картинки 3">
+            <a href="./images/battle.jpg" data-fancybox="group" data-caption="Описание картинки 3">
                 <img src="./images/battle.jpg" alt="" />
             </a>
+
         </div>
         <div>
-            <a href="./images/pInB.png" data-fancybox data-caption="Описание картинки 4">
+            <a href="./images/pInB.png" data-fancybox="group" data-caption="Описание картинки 4">
                 <img src="./images/pInB.png" alt="" />
             </a>
         </div>
